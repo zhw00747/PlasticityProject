eacffd1b9750df20746a704c505ee05c8aa3fbc0
anders <anders.a.resell@gmail.com>
2023-09-24 19:02:23 +0200

    implementing elastic

--- ex1/VUMAT_ex1.f
+++ ex1/VUMAT_ex1.f
@@ -1,16 +1,23 @@
 !-----------------------------------------------------------------------
 !-----------------------------------------------------------------------
-		subroutine assert(condition, message)
+		subroutine assertFunction(condition, message)
   		logical, intent(in) :: condition
   		character(*), intent(in) :: message
-
   		if (.not. condition) then
     		write(*,*) "Assertion failed: ", message
     		stop
 		endif
-
   		return
-		end subroutine assert
+		end subroutine assertFunction
+!-----------------------------------------------------------------------
+! Uncomment this to turn asserts off
+#define DEBUG
+!----------------------------------------------------------------------- 
+#ifdef DEBUG
+#define assert(cond, msg) call assertFunction(cond, msg)
+#else
+#define assert(cond, msg)
+#endif
 !-----------------------------------------------------------------------
 !
 !
@@ -48,40 +55,82 @@
 !-----------------------------------------------------------------------
 !-----Declaration elastic parameters
 !-----------------------------------------------------------------------
-		real*8 E0, nu, lame1, lame2, volStrain !lame1 = mu, and lame2 = lambda
+		real*8 E0, nu, lame1, lame2, epsilon_v !lame1 = lambda, and lame2 = mu
 !-----------------------------------------------------------------------
 !-----Declare other variables
-		integer n,j, ii, ij
+		integer i
+		real*8 t1, t2, t3, t4, t5, t6 !Trial stress components
+!
 !-----------------------------------------------------------------------
 !-----Initialization step (elastic)
+!-----Since the initial values of strain components are zero,  
+!-----strainInc and strain will be equal for this step
 !-----------------------------------------------------------------------
-		call assert((nshr.eq.3),"nshr==3 (plates will be handled later)") 
-      if ((stepTime.eq.totalTime.and.stepTime.eq.zero)) then
+		assert((nshr.eq.3),"nshr==3 (plates will be handled later)")
+      if ((totalTime.eq.zero).and.(stepTime.eq.zero)) then
 			E0 = props(1)
 			nu = props(2)
-			!print *, "E0 ",E0, "nu", nu
-			do n=1,nblock
-				strain_v = strainInc(n,1) + strainInc(n,2) + strainInc(n,3)
-				stressNew(n,1) = lame1 * volStrain + 2. * lame2 * strainInc(n,1)
-				stressNew(n,2) = lame1 * volStrain + 2. * lame2 * strainInc(n,2) 
-				stressNew(n,3) = lame1 * volStrain + 2. * lame2 * strainInc(n,3)  
-				stressNew(n,4) = 2*lame2 *strainInc(n, 4)
-				stressNew(n,5) = 2*lame2 *strainInc(n, 5)
-				stressNew(n,6) = 2*lame2 *strainInc(n, 6)		
+			lame1 = nu * E0 / ((1 + nu)*(1 - 2*nu))
+			lame2 = E0 / (2 * (1 + nu)) 
+			do i = 1,nblock
+				epsilon_v = strainInc(i,1)+strainInc(i,2)+strainInc(i,3)
+				stressNew(i,1) = lame1*epsilon_v + 2.* lame2*strainInc(i,1)
+				stressNew(i,2) = lame1*epsilon_v + 2.* lame2*strainInc(i,2) 
+				stressNew(i,3) = lame1*epsilon_v + 2.* lame2*strainInc(i,3)  
+				stressNew(i,4) = 2.* lame2*strainInc(i, 4)
+				stressNew(i,5) = 2.* lame2*strainInc(i, 5)
+				stressNew(i,6) = 2.* lame2*strainInc(i, 6)		
 			enddo
-
-
+!
 !-----------------------------------------------------------------------
 !-----Ordinary increment
 !-----------------------------------------------------------------------
-         else
+		else
+			do i = 1,nblock
+!-----------------------------------------------------------------------
+!-----Trial stress
+!-----------------------------------------------------------------------
+				epsilon_v = strainInc(i,1) + strainInc(i,2) + strainInc(i,3)
+				t1 = lame1 * epsilon_v + 2. * lame2 * strainInc(i,1)
+				t2 = lame1 * epsilon_v + 2. * lame2 * strainInc(i,2)
+				t3 = lame1 * epsilon_v + 2. * lame2 * strainInc(i,3)
+				t4 = 2. * lame2 *strainInc(i, 4)
+				t5 = 2. * lame2 *strainInc(i, 5)
+				t6 = 2. * lame2 *strainInc(i, 6)
+!
+!-----------------------------------------------------------------------
+!-----Unpack stresses
+!-----------------------------------------------------------------------
+				stressNew(i,1) = t1
+				stressNew(i,2) = t2
+				stressNew(i,3) = t3
+				stressNew(i,4) = t4
+				stressNew(i,5) = t5
+				stressNew(i,6) = t6
+!
+!-----------------------------------------------------------------------
+!-----Update specific internal energy [J/kg]. 
+!-----U_new = U_old + 
+!-----0.5 * (sigma_old_ij + sigma_new_ij) * d_epsilon_ij / rho
+!-----------------------------------------------------------------------			
 
-         endif
+            enerInternNew(i) = enerInternOld(i) + (
+     +          (stressOld(i,1) + stressNew(i,1)) * strainInc(i,1) +
+     +          (stressOld(i,2) + stressNew(i,2)) * strainInc(i,2) +
+     +          (stressOld(i,3) + stressNew(i,3)) * strainInc(i,3) +
+     +     2. * (stressOld(i,4) + stressNew(i,4)) * strainInc(i,4) +
+     +     2. * (stressOld(i,5) + stressNew(i,5)) * strainInc(i,5) +
+     +     2. * (stressOld(i,6) + stressNew(i,6)) * strainInc(i,6)
+     +          ) * 0.5 / density(i)
 
+	  		enddo
+!
+		endif
+!
 !-----------------------------------------------------------------------
 !-----End of subroutine
 !-----------------------------------------------------------------------
-         return
+		return
       end
 
 
